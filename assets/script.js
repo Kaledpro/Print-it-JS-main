81f5c2eb9442d222c6685c89b9a59339fa39e595
kaledpro <bridgekalloo@gmail.com>
2024-09-12 15:31:19 +0200

    modification source et defiller

--- assets/script.js
+++ assets/script.js
@@ -1,109 +1,69 @@
-
+// variables
 
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-
-// arrows and addEventListener
 const arrow_left = document.querySelector('.arrow_left');
-
 const arrow_right = document.querySelector('.arrow_right');
 
-
 let dot = document.querySelectorAll(".dot");
+let dots = document.querySelectorAll(".dots");
 let bannerimg = document.querySelector(".banner-img");
 let bannertitle = document.querySelector(".banner-title");
 let totalslides = slides.length;
 
 let currentIndex = 0;
 
+
+// arrows events
+
 arrow_left.addEventListener("click", function(){
-	if (currentIndex < 0) {
 		currentIndex--;
+	if (currentIndex < 0) {
+		currentIndex = slides.length -1;
 	} 
 	slideshow(currentIndex);
+	activedot();
 	//console.log('Left_Arrow');
 });
 
 arrow_right.addEventListener('click', function(){
-	if (currentIndex >= slides.length) {
 		currentIndex++;
+	if (currentIndex >= slides.length) {
+		currentIndex = 0;
 	}
 	slideshow(currentIndex);
+	activedot();
 	//console.log('Right_Arrow');
 }); 
 
 function slideshow(index) {
 		const currentslide = slides[index];
-		const images = "/assets/images/slide[i]";
-		bannerimg.src =currentslide.assets
+		bannerimg.src = currentslide.image
 		bannertitle.innerHTML = currentslide.tagLine
-		dot.forEach(onedot => onedot.classList.remove('.dot_selected'))
-		dot[index].classList.add('dot_selected')
 }
 
-
-
-/*function slideshow(index) {
-	const imgs = bannerimg.querySelectorAll('img');
-		imgs.forEach((img, index) => {
-		img.classList.toggle('active', index === currentIndex);
-});
-}*/
-
-/*
-function loadimage() {
-	slides.forEach(src => { 
-	const img = document.createElement('img');
-	img.src = src;
-	bannerimg.appendChild(img);
-});
-}
-
-
-function dotload() {
+function activedot() {
 	dot.forEach((dot, index) => {
-	dot.addEventListener("click", () => {
-		currentIndex = index;
-		slideshow(currentIndex);
-});
-});
-}
-*/
-
-//console.log(slides[0]);
-
-function imageshow() {
-	imgs.forEach(src => { 
-	const imgs = document.createElement('img');
-	img.src = src;
-	bannerimg.appendChild(img);
-	});
+	dot.classList.remove('dot_selected');
+	if (index === currentIndex) {
+			dot.classList.add('dot_selected');
 	}
-	
-	/*const dots= document.querySelector('.dots');
-	const dot= document.querySelector('.dot');
-	const add= dots.push('.dot');
-	console.log(add);*/
-
-function setup() {
-	imageshow();
-	dotload();
-	showslide();
+});
 }
